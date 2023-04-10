0cc0e91fce94e35f1959db87d500edf518789c10
Никита <skyrim.skyrim.1001@gmail.com>
2023-04-10 10:59:51 +0700

    Добавьте файлы проекта.

---
+++ Warehouse/BaseWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Warehouse
+{
+    /// <summary>
+    /// Логика взаимодействия для BaseWindow.xaml
+    /// </summary>
+    public partial class BaseWindow : Window
+    {
+        AddingWindow win3 = new AddingWindow();
+        public BaseWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            win3.Show();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Warehouse/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Warehouse
+{
+    public partial class MainWindow : Window
+    {
+        BaseWindow win2 = new BaseWindow();
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if(textbox1.Text != "123456")
+            {
+                MessageBox.Show("Неправильный пароль!!!");
+            }
+            else
+            {
+                win2.Show();
+                this.Close();
+            }
+        }
+    }
+}
